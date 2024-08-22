848f7c976898f4079bbb67da72fd64baefba2569
Guilherme Bezerra <guilherme10alves.ga@gmail.com>
2024-08-22 11:02:41 -0300

    chore: Add environment variables and zod config

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.error(
+    'Environment validation failed', _env.error.format(),
+  )
+
+  throw new Error('Environment validation failed')
+}
+
+export const env = _env.data
