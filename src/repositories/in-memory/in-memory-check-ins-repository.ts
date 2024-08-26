5910ab6751ff06eaf0410c1f33b435dd97374fb6
Guilherme Bezerra <guilherme10alves.ga@gmail.com>
2024-08-26 15:20:00 -0300

    feat: Add findManyByUserId method to CheckInsRepository

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -34,4 +34,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return Promise.resolve(checkInSameDate || null)
   }
+
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
 }
---
+++ src/use-cases/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,73 @@
+import { it, expect, describe, beforeEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+
+import { FetchUserCheckInsHistoryUseCase } from './fetch-user-check-ins-history'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: FetchUserCheckInsHistoryUseCase
+
+describe('Fetch Check-in Hisotry Use Case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new FetchUserCheckInsHistoryUseCase(checkInsRepository)
+  })
+
+  it('should be able to fetch user check-ins history', async () => {
+    await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-01',
+    })
+
+    await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-02',
+    })
+
+    const { checkIns } = await sut.execute({
+      userId: 'user-01',
+      page: 1,
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          user_id: 'user-01',
+          gym_id: 'gym-01',
+        }),
+        expect.objectContaining({
+          user_id: 'user-01',
+          gym_id: 'gym-02',
+        }),
+      ]),
+    )
+  })
+
+  it('should be able to fetch paginated user check-ins history', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await checkInsRepository.create({
+        user_id: 'user-01',
+        gym_id: `gym-${i}`,
+      })
+    }
+
+    const { checkIns } = await sut.execute({
+      userId: 'user-01',
+      page: 2,
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          user_id: 'user-01',
+          gym_id: 'gym-22',
+        }),
+        expect.objectContaining({
+          user_id: 'user-01',
+          gym_id: 'gym-21',
+        }),
+      ]),
+    )
+  })
+})
